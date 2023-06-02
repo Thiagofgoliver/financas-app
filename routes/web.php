a8858db4bade22fbc12b758630f27ccf017e25e5
Thiago Felipe Guimarães oliveira <115421991+Thiagofgoliver@users.noreply.github.com>
2023-06-02 16:09:51 -0300

    atualizado 02/06
    projeto de finanças usando Taravel ,Nodejs,Tailwind

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,20 @@ use Illuminate\Support\Facades\Route;
 
 
  Route::get('/', function () {
-    // return view('welcome');
-    echo '<h1>hello world!!!</h1>';
+    return view('auth.login');
+     
 
      
 
 
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
